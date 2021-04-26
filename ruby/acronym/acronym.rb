ba9b786df8fe38605e604a8b4616ace499b120e2
callbritton <cla.inbox@gmail.com>
2021-04-26 11:32:37 -0400

    Complete Acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -6,3 +6,13 @@ To get started with TDD, see the `README.md` file in your
 `ruby/acronym` directory.
 =end
 
+class Acronym
+  def self.abbreviate(phrase)
+    abr = ""
+    pieces = phrase.gsub(/-/, ' ').split(' ')
+    pieces.each do |piece|
+      abr.concat(piece.slice(0))
+    end
+    abr.upcase
+  end
+end
